c72d16afe25ea66af734255bb90edfde5bd64655
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-13 01:25:42 +0700

    xoa thua

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
