2202c5197abbd0db4226332a6e6869663e23f316
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-13 01:05:22 +0700

    thay theo a an do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br/> Lai Lam Duoc roi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <Link to="/Menu">Menu</Link>
+          <Link to="/Search">Search</Link>
+          <Link to="/FoodShop">Food Shop</Link>
+        </nav>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
