a25769c68c5681a455355b6a2342f38a463e73ff
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-13 01:11:12 +0700

    bỏ browser router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+
         <nav>
           <Link to="/Menu">Menu</Link>
           <Link to="/Search">Search</Link>
@@ -16,7 +16,7 @@ function App() {
           <Route path="/Search" />
           <Route path="/FoodShop" />
         </Routes>
-      </BrowserRouter>
+
     </div>
   );
 }
